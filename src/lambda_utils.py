ce8cf31dcbeb0c1bd3495b6d4a28ff74e2024696
EC2 Default User <ec2-user@ip-10-88-1-53.us-west-2.compute.internal>
2022-08-10 20:28:19 +0000

    getS3path in lambda_utils

--- src/lambda_utils.py
+++ src/lambda_utils.py
@@ -1,3 +1,4 @@
+import os
 
 def getParameter( param_dict, k, v_default ):
     """ Return value of key k in param_dict, if found - otherwise return v_default.
@@ -7,3 +8,42 @@ def getParameter( param_dict, k, v_default ):
     else:
         return v_default
 
+def getS3path( partialFilePaths ):
+    """ Given a list of partial input file paths (comma-separated string or list),
+        prepends the S3 bucket name.
+        Return full file paths in the same type provided as input.
+
+    >>> lambda_utils.getS3path('hubseq/test/file1.pdf')
+    's3://hubtenants/hubseq/test/file1.pdf'
+    >>> lambda_utils.getS3path('hubseq/test/file1.pdf,hubseq/file2.txt')
+    's3://hubtenants/hubseq/test/file1.pdf,s3://hubtenants/hubseq/file2.txt'
+    >>> lambda_utils.getS3path(['hubseq/test/file1.pdf'])
+    ['s3://hubtenants/hubseq/test/file1.pdf']
+    >>> lambda_utils.getS3path(['hubseq/test/file1.pdf', 'hubseq/file2.txt'])
+    ['s3://hubtenants/hubseq/test/file1.pdf', 's3://hubtenants/hubseq/file2.txt']
+    """
+    TEAM_BUCKET = 's3://hubtenants/'
+
+    # create list of partial file paths from input
+    if type(partialFilePaths)==type(''):
+        partialFilePathsList = partialFilePaths.split(',')
+        returnType = "string"
+    elif  type(partialFilePaths)==type([]):
+        partialFilePathsList = partialFilePaths
+        returnType = "list"
+    else:
+        partialFilePathsList = []
+        returnType = "list"
+
+    # create full filepaths
+    fullPaths = []
+    for f in partialFilePathsList:
+        if not f.startswith('s3://'):
+            fullPaths.append(os.path.join(TEAM_BUCKET, f.lstrip('/')))
+
+    # format and return full filepaths
+    if returnType=="string":
+        return ','.join(fullPaths)
+    else:
+        return fullPaths
+    
